8959d4a8c3a9b1945b2e18a327af2c6d2af8efc0
DaWood32913 <woodleyjohnson@yahoo.com>
2019-07-20 09:06:34 -0400

    working on routing

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 //Dependencies
 var express = require ('express');
-var bodyParser = require ('body-parser');
 var path = require ('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
