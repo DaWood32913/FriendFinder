57f625d937072e4a9d8aaa722f2e1a3e1b229f57
DaWood32913 <woodleyjohnson@yahoo.com>
2019-07-21 15:41:18 -0400

    fixed module export

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,4 +4,8 @@ module.exports = function(app){
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname, '/../public/survey.html'));
     });
+
+    app.get('/home', function(req,res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
 };
