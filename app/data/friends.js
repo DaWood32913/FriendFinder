5fec971554982e5747ecb8f92f57d1f15a4df9f2
DaWood32913 <woodleyjohnson@yahoo.com>
2019-07-24 14:20:51 -0400

    completed friend match array

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+    {
+      name: "Flavor Flav",
+      photo: "https://www.thefamouspeople.com/profiles/images/og-flavor-flav-4001.jpg ",
+      scores: ["2", "2", "2", "2", "2","2", "2", "2", "2", "2"]
+    }, {
+      name: "Mark Wahlberg",
+      photo: "https://pmcdeadline2.files.wordpress.com/2018/03/mark-wahlberg-e1561747029189.jpg?w=681&h=383&crop=1 ",
+      scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    }, {
+      name: " Jussie Smollett",
+      photo: "http://images.tritondigitalcms.com/6616/sites/356/2019/02/21111509/Jussie-Smollett-convicted.jpg ",
+      scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    }, {
+      name: "Woodley Johnson",
+      photo: "https://scontent.fphl1-2.fna.fbcdn.net/v/t1.0-9/378439_1567451869627_1839514310_n.jpg?_nc_cat=107&_nc_oc=AQmc74igUGnYX0FFwmthPr55szmbjEZPuHwJUI095kEwvu0OHZxAEnimWI4hHT26Rz0&_nc_ht=scontent.fphl1-2.fna&oh=b19ad145018962875413ce8ca0bfb1c9&oe=5DB760AB ",
+      scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    },
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
