c2e4264a5d5be61fa716d266947c83cd99127882
DaWood32913 <woodleyjohnson@yahoo.com>
2019-07-24 19:53:47 -0400

    serve html

--- server.js
+++ server.js
@@ -19,3 +19,7 @@ require('./app/routing/htmlRoutes')(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
